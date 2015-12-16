78a6696049c417922ae103da11cf4260e0f1200a
elk <marcel.au@web.de>
2015-12-16 10:10:01 +0100

    Cleaned up QuickFixes.

--- com.eco.bio7.redit/src/com/eco/bio7/reditor/code/RAssistProcessor.java
+++ com.eco.bio7.redit/src/com/eco/bio7/reditor/code/RAssistProcessor.java
@@ -1,12 +1,8 @@
 package com.eco.bio7.reditor.code;
 
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
-
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
 import org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
@@ -16,9 +12,8 @@ import org.eclipse.ui.texteditor.MarkerAnnotation;
 
 public class RAssistProcessor implements IQuickAssistProcessor {
 
-	// @Override
 	public String getErrorMessage() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
@@ -36,20 +31,19 @@ public class RAssistProcessor implements IQuickAssistProcessor {
 					}
 				}
 			} catch (CoreException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
 		}
 
-		// String text=annotation.getText();
 		/* Text is not NA! */
 		if (text != null && text.startsWith("Err")) {
 			// errorCode = text;
 			return true;
 		}
-
+		/* Text is not NA! */
 		else if (text != null && text.startsWith("Warn")) {
-			// errorCode = text;
+
 			return true;
 		}
 
@@ -62,7 +56,6 @@ public class RAssistProcessor implements IQuickAssistProcessor {
 	@Override
 	public boolean canAssist(IQuickAssistInvocationContext invocationContext) {
 
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -70,9 +63,9 @@ public class RAssistProcessor implements IQuickAssistProcessor {
 	public ICompletionProposal[] computeQuickAssistProposals(IQuickAssistInvocationContext invocationContext) {
 		ISourceViewer viewer = invocationContext.getSourceViewer();
 		int offset = invocationContext.getOffset();
-		String text = getCompleteText(viewer.getDocument(), offset);
+		// String text = getCompleteText(viewer.getDocument(), offset);
 		ICompletionProposal[] prop = null;
-
+		RQuickFixSolutions solutions = new RQuickFixSolutions();
 		Iterator<?> it = viewer.getAnnotationModel().getAnnotationIterator();
 
 		while (it.hasNext()) {
@@ -89,7 +82,7 @@ public class RAssistProcessor implements IQuickAssistProcessor {
 				if (startChar > 0 && endChar > 0 && offset <= endChar && offset >= startChar) {
 					try {
 						String errorProposal = (String) (marker.getAttribute(IMarker.TEXT));
-						prop = theProposals(errorProposal, offset, endChar, prop);
+						prop = solutions.getProposals(errorProposal, offset, endChar, prop);
 					} catch (CoreException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -103,170 +96,50 @@ public class RAssistProcessor implements IQuickAssistProcessor {
 
 	}
 
-	private ICompletionProposal[] theProposals(String errorCode, int offset, int endChar, ICompletionProposal[] prop) {
-		if (errorCode != null) {
-			switch (errorCode) {
-			case "Err1":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-				break;
-			case "Err2":
-
-				break;
-			case "Err3":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-				break;
-			case "Err4":
-
-				break;
-			case "Err5":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-				break;
-			case "Err6":
-
-				break;
-			case "Err7":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-				break;
-			case "Err8":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ']'", offset, endChar, "", 1) };
-
-				break;
-			case "Err9":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ']'", offset, endChar, "", 1) };
-				break;
-			case "Err10":
-
-				break;
-			case "Err11":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove '}'", offset, endChar, "", 1) };
-				break;
-			case "Warn12":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace 'true' with 'TRUE'", offset, endChar, "TRUE", 4) };
-
-				break;
-			case "Warn13":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace 'false' with 'FALSE'", offset, endChar, "FALSE", 5) };
-
-				break;
-			case "Warn14":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace 'null' with 'NULL'", offset, endChar, "NULL", 4) };
-
-				break;
-			case "Warn15":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace 'na' with 'NA'", offset, endChar, "NA", 2) };
-
-				break;
-			case "Err16":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-
-				break;
-			case "Err17":
-
-				break;
-			case "Err18":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
-				break;
-			case "Err19":
-
-				break;
-			case "Err20":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace '=>' with '>='", offset, endChar, ">=", 2) };
-				break;
-			case "Err21":
-				prop = new ICompletionProposal[] {
-
-						new QuickFixCompletionProposal("Replace '=<' with '<='", offset, endChar, "<=", 2) };
-				break;
-
-			default:
-				break;
-			}
-			/*
-			 * if (errorCode.equals("Err7")) { prop = new ICompletionProposal[]
-			 * {
-			 * 
-			 * new QuickFixCompletionProposal("Remove", offset, text,
-			 * ""),
-			 * 
-			 * // new // QuickFixCompletionProposal(
-			 * "This is the second proposal!",offset,text,"")
-			 * 
-			 * }; }
-			 */
-		}
-		return prop;
-	}
-
-	private String getCompleteText(IDocument document, int currentOffset) {
-
-		int count = currentOffset;
-
-		String tobeCompleteText = "";
-
-		try {
-
-			while (--count >= 0) {
-
-				char c = ' ';
-
-				c = document.getChar(count);
-
-				if (isWhiteSpace(c)) {
-
-					break;
-
-				}
-
-			}
-
-			tobeCompleteText = document.get(count + 1, currentOffset - (count + 1));
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
-		}
-
-		return tobeCompleteText;
-
-	}
-
-	private boolean isWhiteSpace(char c) {
-
-		if (c == '\n' || c == '\t' || c == ' ')
-
-			return true;
-
-		return false;
-
-	}
+	/*
+	 * private String getCompleteText(IDocument document, int currentOffset) {
+	 * 
+	 * int count = currentOffset;
+	 * 
+	 * String tobeCompleteText = "";
+	 * 
+	 * try {
+	 * 
+	 * while (--count >= 0) {
+	 * 
+	 * char c = ' ';
+	 * 
+	 * c = document.getChar(count);
+	 * 
+	 * if (isWhiteSpace(c)) {
+	 * 
+	 * break;
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * tobeCompleteText = document.get(count + 1, currentOffset - (count + 1));
+	 * 
+	 * } catch (Exception e) {
+	 * 
+	 * e.printStackTrace();
+	 * 
+	 * }
+	 * 
+	 * return tobeCompleteText;
+	 * 
+	 * }
+	 * 
+	 * private boolean isWhiteSpace(char c) {
+	 * 
+	 * if (c == '\n' || c == '\t' || c == ' ')
+	 * 
+	 * return true;
+	 * 
+	 * return false;
+	 * 
+	 * }
+	 */
 
 }
---
+++ com.eco.bio7.redit/src/com/eco/bio7/reditor/code/RQuickFixSolutions.java
@@ -0,0 +1,119 @@
+package com.eco.bio7.reditor.code;
+
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+
+public class RQuickFixSolutions  {
+	
+	public ICompletionProposal[] getProposals(String errorCode, int offset, int endChar, ICompletionProposal[] prop) {
+		if (errorCode != null) {
+			switch (errorCode) {
+			case "Err1":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+				break;
+			case "Err2":
+
+				break;
+			case "Err3":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+				break;
+			case "Err4":
+
+				break;
+			case "Err5":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+				break;
+			case "Err6":
+
+				break;
+			case "Err7":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+				break;
+			case "Err8":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ']'", offset, endChar, "", 1) };
+
+				break;
+			case "Err9":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ']'", offset, endChar, "", 1) };
+				break;
+			case "Err10":
+
+				break;
+			case "Err11":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove '}'", offset, endChar, "", 1) };
+				break;
+			case "Warn12":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace 'true' with 'TRUE'", offset, endChar, "TRUE", 4) };
+
+				break;
+			case "Warn13":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace 'false' with 'FALSE'", offset, endChar, "FALSE", 5) };
+
+				break;
+			case "Warn14":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace 'null' with 'NULL'", offset, endChar, "NULL", 4) };
+
+				break;
+			case "Warn15":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace 'na' with 'NA'", offset, endChar, "NA", 2) };
+
+				break;
+			case "Err16":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+
+				break;
+			case "Err17":
+
+				break;
+			case "Err18":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Remove ')'", offset, endChar, "", 1) };
+				break;
+			case "Err19":
+
+				break;
+			case "Err20":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace '=>' with '>='", offset, endChar, ">=", 2) };
+				break;
+			case "Err21":
+				prop = new ICompletionProposal[] {
+
+						new RQuickFixCompletionProposal("Replace '=<' with '<='", offset, endChar, "<=", 2) };
+				break;
+
+			default:
+				break;
+			}
+			
+		}
+		return prop;
+	}
+	
+     
+   }
