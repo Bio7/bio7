c0eb15bc5c991b3f5853b9617340fbca65516cd7
elk <marcel.au@web.de>
2016-01-28 14:25:40 +0100

    Refactored out duplicate methods.

--- com.eco.bio7.redit/src/com/eco/bio7/reditor/actions/ExtractVariable.java
+++ com.eco.bio7.redit/src/com/eco/bio7/reditor/actions/ExtractVariable.java
@@ -1,14 +1,10 @@
 package com.eco.bio7.reditor.actions;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.BufferedTokenStream;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.TokenStreamRewriter;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
@@ -22,25 +18,17 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
-import com.eco.bio7.reditor.antlr.RFilter;
-import com.eco.bio7.reditor.antlr.RLexer;
-import com.eco.bio7.reditor.antlr.RParser;
-import com.eco.bio7.reditor.antlr.RParser.ProgContext;
-import com.eco.bio7.reditor.antlr.refactor.ExtractInterfaceListener;
-import com.eco.bio7.reditor.antlr.refactor.ParseErrorListener;
 import com.eco.bio7.reditor.antlr.refactor.RefactorDialog;
+import com.eco.bio7.reditor.antlr.refactor.RefactorDocUtil;
+import com.eco.bio7.reditor.antlr.refactor.RefactorParse;
 import com.eco.bio7.util.Util;
-import org.antlr.v4.runtime.TokenStream;
 
 public class ExtractVariable implements IEditorActionDelegate {
 
 	private ISelection selection;
 	private IEditorPart targetEditor;
-	private CommonTokenStream tokens;
-	private RParser parser;
 	private TokenStreamRewriter rewriter;
-	private ProgContext tree;
-	private BufferedTokenStream bufferTokenStream;
+
 	private boolean global;
 
 	public void setActiveEditor(final IAction action, final IEditorPart targetEditor) {
@@ -69,9 +57,9 @@ public class ExtractVariable implements IEditorActionDelegate {
 				errorMessage("Nothing selected!");
 				return;
 			}
-
+			RefactorParse parse = new RefactorParse();
 			/* First parse of the selection! */
-			boolean errors = parseSource(text);
+			boolean errors = parse.parseSource(text);
 			if (errors == false) {
 
 				RefactorDialog dlg = new RefactorDialog(Display.getCurrent().getActiveShell());
@@ -86,19 +74,19 @@ public class ExtractVariable implements IEditorActionDelegate {
 			} else {
 				errorMessage("Parser error occured!\nPlease select valid R expressions!");
 				/* Revert changes if parser has errors! */
-				//doc.set(docText);
+				// doc.set(docText);
 				// System.out.println("How many errors1: " +
 				// parser.getNumberOfSyntaxErrors());
 				return;
 			}
 			/* Second parse of the whole text without the selected text! */
 			String fullTextWithoutSel = doc.get();
-			boolean error = parseSource(fullTextWithoutSel);
+			boolean error = parse.parseSource(fullTextWithoutSel);
 
 			/* Extract selection offset! */
 			int selectionOffset = selection.getOffset();
 			if (error == false) {
-				rewriter = new TokenStreamRewriter(tokens);
+				rewriter = new TokenStreamRewriter(parse.getTokens());
 
 				// Token token = tokens.get(0);
 				Token startToken = null;
@@ -125,13 +113,11 @@ public class ExtractVariable implements IEditorActionDelegate {
 						StringBuffer str = new StringBuffer();
 						/* if the doc has no linebreak at the end! */
 
-						
-
 						str.append(varName);
 						str.append(" <- ");
 						str.append(text);
 						str.append(System.getProperty("line.separator"));
-						rewriter.insertBefore(tree.start, str.toString());
+						rewriter.insertBefore(parse.getTree().start, str.toString());
 						/*
 						 * We replace the selected text with the
 						 * TokenStreamRewriter!
@@ -139,12 +125,10 @@ public class ExtractVariable implements IEditorActionDelegate {
 
 					} else {
 
-						
-
-						int ws = getLeadingWhitespaceNumber(selectionOffset, doc);
+						int ws = RefactorDocUtil.getLeadingWhitespaceNumber(selectionOffset, doc);
 						/* Set function local! */
 						StringBuffer str = new StringBuffer();
-						
+
 						if (ws > 0) {
 							str.append(String.format("%-" + ws + "s", ""));
 						}
@@ -186,11 +170,11 @@ public class ExtractVariable implements IEditorActionDelegate {
 			} else {
 				errorMessage("Parser error occured!\nPlease select valid R expressions!");
 				/* Revert changes if parser has errors! */
-				//doc.set(docText);
+				// doc.set(docText);
 				return;
 			}
 			/* Third parse for the final result! */
-			boolean errorNewText = parseSource(rewriter.getText());
+			boolean errorNewText = parse.parseSource(rewriter.getText());
 
 			if (errorNewText == false) {
 				/* Write to the editor! */
@@ -207,40 +191,6 @@ public class ExtractVariable implements IEditorActionDelegate {
 
 		}
 
-	}
-
-	/* Here we parse the text and test for possible errors! */
-	private boolean parseSource(String fullText) {
-		boolean errors;
-		ANTLRInputStream input = new ANTLRInputStream(fullText);
-		RLexer lexer = new RLexer(input);
-		tokens = new CommonTokenStream(lexer);
-		bufferTokenStream = new BufferedTokenStream(lexer);
-		RFilter filter = new RFilter(tokens);
-		filter.removeErrorListeners();
-		filter.stream(); // call start rule: stream
-		tokens.reset();
-		ParseErrorListener parseErrorListener = new ParseErrorListener();
-		parser = new RParser(tokens);
-
-		lexer.removeErrorListeners();
-		// lexer.addErrorListener(li);
-		parser.removeErrorListeners();
-
-		parser.addErrorListener(parseErrorListener);
-
-		tree = parser.prog();
-		ParseTreeWalker walker = new ParseTreeWalker(); // create standard
-														// walker
-		ExtractInterfaceListener extractor = new ExtractInterfaceListener(tokens, parser);
-		walker.walk(extractor, tree);
-
-		if (parser.getNumberOfSyntaxErrors() == 0) {
-			errors = false;
-		} else {
-			errors = true;
-		}
-		return errors;
 	}
 
 	public void selectionChanged(final IAction action, final ISelection selection) {
@@ -265,56 +215,6 @@ public class ExtractVariable implements IEditorActionDelegate {
 				messageBox.open();
 			}
 		});
-	}
-	/*
-	 * The following methods from class: XmlDocumentFormatter in package:
-	 * org.eclipse.ant.internal.ui.editor.formatter;
-	 * *************************************************************************
-	 * ****** Copyright (c) 2004, 2006s John-Mason P. Shackelford and others.
-	 * All rights reserved. This program and the accompanying materials are made
-	 * available under the terms of the Eclipse Public License v1.0 which
-	 * accompanies this distribution, and is available at
-	 * http://www.eclipse.org/legal/epl-v10.html
-	 * 
-	 * Contributors: John-Mason P. Shackelford - initial API and implementation
-	 * IBM Corporation - bug fixes
-	 *******************************************************************************/
-
-	public static StringBuffer getLeadingWhitespace(int offset, IDocument document) {
-		StringBuffer indent = new StringBuffer();
-		try {
-			org.eclipse.jface.text.IRegion line = document.getLineInformationOfOffset(offset);
-			int lineOffset = line.getOffset();
-			int nonWS = findEndOfWhiteSpace(document, lineOffset, lineOffset + line.getLength());
-			indent.append(document.get(lineOffset, nonWS - lineOffset));
-			return indent;
-		} catch (BadLocationException e) {
-			return indent;
-		}
-	}
-
-	public static int getLeadingWhitespaceNumber(int offset, IDocument document) {
-		int indent = 0;
-		try {
-			org.eclipse.jface.text.IRegion line = document.getLineInformationOfOffset(offset);
-			int lineOffset = line.getOffset();
-			int nonWS = findEndOfWhiteSpace(document, lineOffset, lineOffset + line.getLength());
-			indent = nonWS - lineOffset;
-			return indent;
-		} catch (BadLocationException e) {
-			return indent;
-		}
-	}
-
-	public static int findEndOfWhiteSpace(IDocument document, int offset, int end) throws BadLocationException {
-		while (offset < end) {
-			char c = document.getChar(offset);
-			if (c != ' ' && c != '\t') {
-				return offset;
-			}
-			offset++;
-		}
-		return end;
 	}
 
 }
