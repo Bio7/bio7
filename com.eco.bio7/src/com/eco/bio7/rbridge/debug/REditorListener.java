de68158e484a689523dd4d0195f031b17770de90
elk <marcel.au@web.de>
2018-05-25 13:33:25 +0200

    Changed

--- com.eco.bio7/src/com/eco/bio7/rbridge/debug/REditorListener.java
+++ com.eco.bio7/src/com/eco/bio7/rbridge/debug/REditorListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2014 M. Austenfeld
+ * Copyright (c) 2004-2018 M. Austenfeld
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
