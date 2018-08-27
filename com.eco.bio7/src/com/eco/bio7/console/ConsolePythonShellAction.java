5c09b931839327c5b5732baa99876e4c00a62e37
elk <marcel.au@web.de>
2018-08-27 15:41:27 +0200

    Changed

--- com.eco.bio7/src/com/eco/bio7/console/ConsolePythonShellAction.java
+++ com.eco.bio7/src/com/eco/bio7/console/ConsolePythonShellAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2012 M. Austenfeld
+ * Copyright (c) 2005-2018 M. Austenfeld
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,9 +13,6 @@ package com.eco.bio7.console;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
-
 import com.eco.bio7.Bio7Plugin;
 
 public class ConsolePythonShellAction extends Action {
@@ -26,7 +23,8 @@ public class ConsolePythonShellAction extends Action {
 		setId("Python");
 		setText("Python");
 
-		ImageDescriptor desc = ImageDescriptor.createFromImage(Bio7Plugin.getImageDescriptor("/icons/views/pythonaction.png").createImage());
+		ImageDescriptor desc = ImageDescriptor
+				.createFromImage(Bio7Plugin.getImageDescriptor("/icons/views/pythonaction.png").createImage());
 
 		this.setImageDescriptor(desc);
 	}
