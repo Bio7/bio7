14c3f1bd13460be6faf3d705edc7ef971ec3b17b
elk <marcel.au@web.de>
2018-10-17 13:24:40 +0200

    Add Jython for Java 11

--- com.eco.bio7/src/com/eco/bio7/scriptengines/ScriptEngineConnection.java
+++ com.eco.bio7/src/com/eco/bio7/scriptengines/ScriptEngineConnection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2013 M. Austenfeld
+ * Copyright (c) 2004-2018 M. Austenfeld
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,19 +12,25 @@ package com.eco.bio7.scriptengines;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-
-import org.python.util.PythonInterpreter;
+import org.python.core.Options;
 
 /**
  * @author Bio7 A class to return the Java Scripting connections of Bio7.
  */
 public class ScriptEngineConnection {
-
+	static {
+		/*
+		 * Workaround from:
+		 * http://python.6.x6.nabble.com/2-7b3-JSR-223-ScriptEngine-null-b2-works-
+		 * td5074755.html
+		 */
+		Options.importSite = false;
+	}
 	private static ScriptEngine groovyShell = new ScriptEngineManager().getEngineByName("groovy");
 	private static ScriptEngine scriptingEnginePython = new ScriptEngineManager().getEngineByName("python");
 	private static ScriptEngine scriptingEngineBeanShell = new ScriptEngineManager().getEngineByName("beanshell");
 	private static ScriptEngine scriptingEngineJavaScript = new ScriptEngineManager().getEngineByName("Nashorn");
-	
+
 	/**
 	 * Get the current Bio7 Groovy connection.
 	 * 
@@ -52,7 +58,7 @@ public class ScriptEngineConnection {
 	public static ScriptEngine getScriptingEngineBeanShell() {
 		return scriptingEngineBeanShell;
 	}
-	
+
 	/**
 	 * Get the current Bio7 BeanShell connection.
 	 * 
@@ -82,7 +88,7 @@ public class ScriptEngineConnection {
 	public static void reinitializeJython() {
 		scriptingEnginePython = new ScriptEngineManager().getEngineByName("python");
 	}
-	
+
 	/**
 	 * Reinitializes the Jython interpreter.
 	 */
