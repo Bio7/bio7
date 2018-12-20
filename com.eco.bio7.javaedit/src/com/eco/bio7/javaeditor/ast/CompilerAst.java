44f0d05ccd66aa59410a2065c6730de2baeb951d
elk <marcel.au@web.de>
2018-12-20 15:55:53 +0100

    Changed

--- com.eco.bio7.javaedit/src/com/eco/bio7/javaeditor/ast/CompilerAst.java
+++ com.eco.bio7.javaedit/src/com/eco/bio7/javaeditor/ast/CompilerAst.java
@@ -35,10 +35,10 @@ public class CompilerAst {
 
 	public CompilerAst(String source) {
 		Document doc = new Document(source);
-		parser = ASTParser.newParser(AST.JLS10);
+		parser = ASTParser.newParser(AST.JLS11);
 
 		Map<String, String> options = JavaCore.getOptions();
-		JavaCore.setComplianceOptions(JavaCore.VERSION_10, options);
+		JavaCore.setComplianceOptions(JavaCore.VERSION_11, options);
 		parser.setCompilerOptions(options);
 
 		parser.setResolveBindings(true);
