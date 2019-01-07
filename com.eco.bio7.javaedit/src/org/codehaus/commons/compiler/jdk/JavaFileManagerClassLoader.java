4b9330a2eca93500d0c139307954e472fdfe8cbd
elk <marcel.au@web.de>
2019-01-07 13:49:24 +0100

    Changed

--- com.eco.bio7.javaedit/src/org/codehaus/commons/compiler/jdk/JavaFileManagerClassLoader.java
+++ com.eco.bio7.javaedit/src/org/codehaus/commons/compiler/jdk/JavaFileManagerClassLoader.java
@@ -1,4 +1,3 @@
-
 /*
  * Janino - An embedded Java[TM] compiler
  *
@@ -23,7 +22,7 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+ 
 package org.codehaus.commons.compiler.jdk;
 
 import java.io.ByteArrayInputStream;
