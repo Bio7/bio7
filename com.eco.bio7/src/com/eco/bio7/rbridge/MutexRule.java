b0f5c0dc2d0454eeee37337854443ac1e867458e
Bio7 <marcel.au@web.de>
2021-04-13 17:37:28 +0200

    Changed

--- com.eco.bio7/src/com/eco/bio7/rbridge/MutexRule.java
+++ com.eco.bio7/src/com/eco/bio7/rbridge/MutexRule.java
@@ -1,12 +1,9 @@
-package com.eco.bio7.rbridge;
-
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-
-public class MutexRule implements ISchedulingRule {
-      public boolean isConflicting(ISchedulingRule rule) {
-         return rule == this;
-      }
-      public boolean contains(ISchedulingRule rule) {
-         return rule == this;
-      }
-   }
+/*
+ * package com.eco.bio7.rbridge;
+ * 
+ * import org.eclipse.core.runtime.jobs.ISchedulingRule;
+ * 
+ * public class MutexRule implements ISchedulingRule { public boolean
+ * isConflicting(ISchedulingRule rule) { return rule == this; } public boolean
+ * contains(ISchedulingRule rule) { return rule == this; } }
+ */
