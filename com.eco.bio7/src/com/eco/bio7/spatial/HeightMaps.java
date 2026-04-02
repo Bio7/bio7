760c5608e3dbeb68c2bcb0f760af63f9c0c4fae3
 Bio7 <marcel.au@web.de>
2026-04-02 16:39:47 +0200

    Improved the 3D HeightMap rendering to use smooth shading.

--- com.eco.bio7/src/com/eco/bio7/spatial/HeightMaps.java
+++ com.eco.bio7/src/com/eco/bio7/spatial/HeightMaps.java
@@ -11,7 +11,6 @@
  *     M. Austenfeld
  *******************************************************************************/
 
-
 package com.eco.bio7.spatial;
 
 import ij.ImagePlus;
@@ -20,7 +19,8 @@ import ij.measure.Calibration;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.TextureCoords;
-import static com.jogamp.opengl.GL2.*; // GL2 constants
+import static com.jogamp.opengl.GL2.*;
+import java.nio.FloatBuffer;
 
 public class HeightMaps {
 	public SpatialStructure grid;
@@ -43,6 +43,10 @@ public class HeightMaps {
 	float[][] heights;
 	private static boolean inverse = false;
 
+	// Smooth shading variables
+	private float[][][] vertexNormals;
+	private int lastDetailUsed = -1;
+
 	public static boolean isInverse() {
 		return inverse;
 	}
@@ -55,33 +59,130 @@ public class HeightMaps {
 	private static boolean dynamicMap = true;
 
 	public HeightMaps() {
-
 		this.grid = SpatialStructure.getSpatialStructureInstance();
-
 	}
 
-	public void RenderHeightMap(Texture text, GL2 gl) {
-
-		int x, y;
-		float z = 0;
-
-		text.enable(gl);
-		text.bind(gl);
-
-		float a = 1.0f;
-
-		int cx = -OriginX;// Cartesian correction for the height!
+	/**
+	 * Compute vertex normals for smooth shading by averaging normals from all
+	 * surrounding triangles
+	 */
+	public void computeVertexNormals(Texture text, GL2 gl) {
+		int cx = -OriginX;
 		int cy = -OriginY;
 		float mX = text.getWidth();
 		float mY = text.getHeight();
 
-		float s = detail;// How fine is the resolution of the quads.
+		// Initialize vertex normals - grid-based indexing
+		int gridWidth = (int) ((mX / detail) + 1);
+		int gridHeight = (int) ((mY / detail) + 1);
+		vertexNormals = new float[gridHeight][gridWidth][3];
 
-		float tileX;
-		float tileY;
-		/* Graphics Card specific? */
-		float tilestepX;
-		float tilestepY;
+		// Initialize all normals to zero
+		for (int y = 0; y < gridHeight; y++) {
+			for (int x = 0; x < gridWidth; x++) {
+				vertexNormals[y][x][0] = 0;
+				vertexNormals[y][x][1] = 0;
+				vertexNormals[y][x][2] = 0;
+			}
+		}
+
+		float[] vA = new float[3];
+		float[] vB = new float[3];
+		float[] vC = new float[3];
+		float[] vD = new float[3];
+
+		int gridX = 0, gridY = 0;
+
+		// Accumulate normals from all triangles
+		for (int Y = OriginY; Y < (text.getHeight() - detail) + OriginY; Y += detail) {
+			gridX = 0;
+			for (int X = OriginX; X < (text.getWidth() - detail) + OriginX; X += detail) {
+
+				// Get quad vertices
+				vA[0] = X;
+				vA[1] = Y;
+				vA[2] = Height(X + cx, Y + cy) * scale;
+
+				vB[0] = X;
+				vB[1] = Y + detail;
+				vB[2] = Height(X + cx, Y + detail + cy) * scale;
+
+				vC[0] = X + detail;
+				vC[1] = Y + detail;
+				vC[2] = Height(X + detail + cx, Y + detail + cy) * scale;
+
+				vD[0] = X + detail;
+				vD[1] = Y;
+				vD[2] = Height(X + detail + cx, Y + cy) * scale;
+
+				float[] n1 = calculateNormals(vA, vB, vC);
+				float[] n2 = calculateNormals(vA, vC, vD);
+
+				// Add normals to grid vertices
+				if (gridY < gridHeight && gridX < gridWidth) {
+					vertexNormals[gridY][gridX][0] += n1[0] + n2[0];
+					vertexNormals[gridY][gridX][1] += n1[1] + n2[1];
+					vertexNormals[gridY][gridX][2] += n1[2] + n2[2];
+				}
+				if (gridY + 1 < gridHeight && gridX < gridWidth) {
+					vertexNormals[gridY + 1][gridX][0] += n1[0];
+					vertexNormals[gridY + 1][gridX][1] += n1[1];
+					vertexNormals[gridY + 1][gridX][2] += n1[2];
+				}
+				if (gridY + 1 < gridHeight && gridX + 1 < gridWidth) {
+					vertexNormals[gridY + 1][gridX + 1][0] += n1[0] + n2[0];
+					vertexNormals[gridY + 1][gridX + 1][1] += n1[1] + n2[1];
+					vertexNormals[gridY + 1][gridX + 1][2] += n1[2] + n2[2];
+				}
+				if (gridY < gridHeight && gridX + 1 < gridWidth) {
+					vertexNormals[gridY][gridX + 1][0] += n2[0];
+					vertexNormals[gridY][gridX + 1][1] += n2[1];
+					vertexNormals[gridY][gridX + 1][2] += n2[2];
+				}
+
+				gridX++;
+			}
+			gridY++;
+		}
+
+		// Normalize all vertex normals
+		for (int y = 0; y < gridHeight; y++) {
+			for (int x = 0; x < gridWidth; x++) {
+				normalize(vertexNormals[y][x]);
+			}
+		}
+
+		lastDetailUsed = detail;
+	}
+
+	private float[] getGridNormal(int gridY, int gridX) {
+		if (vertexNormals != null && gridY >= 0 && gridY < vertexNormals.length && gridX >= 0
+				&& gridX < vertexNormals[0].length) {
+			return vertexNormals[gridY][gridX];
+		}
+		return new float[] { 0, 0, 1 };
+	}
+
+	/**
+	 * Faster VBO-based rendering method with smooth shading (preferred)
+	 */
+	public void RenderHeightMap(Texture text, GL2 gl) {
+		// Recompute normals if detail changed
+		if (vertexNormals == null || lastDetailUsed != detail) {
+			computeVertexNormals(text, gl);
+		}
+
+		text.enable(gl);
+		text.bind(gl);
+
+		int cx = -OriginX;
+		int cy = -OriginY;
+		float mX = text.getWidth();
+		float mY = text.getHeight();
+
+		float s = detail;
+
+		float tileX, tileY, tilestepX, tilestepY;
 		if (graphicsCardSwitch == false) {
 			tileX = (mX / s);
 			tileY = (mY / s);
@@ -94,163 +195,111 @@ public class HeightMaps {
 			tilestepY = (mY / s) / s;
 		}
 
-		float tilex = 0;
-		float tiley = 0;
-		float i = 0;// Counter for the steps and multiplier for coordinates.
-		float u = 0;
-		int heightCount = 0;
-		text.bind(gl);
-		text.enable(gl);
+		// Calculate buffer sizes
+		int quadCount = (int) ((mX / s) * (mY / s));
+		int vertexCount = quadCount * 6; // 6 vertices per quad (2 triangles)
 
-		if (bRender) {
-			gl.glBegin(GL_TRIANGLES); // Render triangles
-		} else {
-			gl.glBegin(GL_LINES);
-		}
+		FloatBuffer vertexBuffer = FloatBuffer.allocate(vertexCount * 3);
+		FloatBuffer normalBuffer = FloatBuffer.allocate(vertexCount * 3);
+		FloatBuffer texCoordBuffer = FloatBuffer.allocate(vertexCount * 2);
+
+		int gridX = 0, gridY = 0;
+		float tilex = 0, tiley = 0;
+		float i = 0, u = 0;
 
 		for (int Y = OriginY; Y <= (text.getHeight() - detail) + OriginY; Y += detail) {
 			tiley = (tileY * u) / tilestepY;
-			heightCount = 0;
+			gridX = 0;
 			for (int X = OriginX; X <= (text.getWidth() - detail) + OriginX; X += detail) {
 				tilex = (tileX * i) / tilestepX;
-				heightCount++;
-				// Get The (X, Y, Z) Value For The Bottom Left Vertex
-				x = X;
-				y = Y;
-				if (cartesian) {
-					z = Height(X + cx, Y + cy);
-				} else {
-					z = Height(X, Y);
-				}
 
-				/* Store the calculated coordinates */
-				v1[0] = x;
-				v1[1] = y;
-				v1[2] = z * scale;
-				// Get The (X, Y, Z) Value For The Top Left Vertex
-				x = X;
-				y = Y + detail;
-				if (cartesian) {
-					z = Height(X + cx, Y + detail + cy);
+				float z1 = Height(X + cx, Y + cy) * scale;
+				float z2 = Height(X + cx, Y + detail + cy) * scale;
+				float z3 = Height(X + detail + cx, Y + detail + cy) * scale;
+				float z4 = Height(X + detail + cx, Y + cy) * scale;
 
-				} else {
-					z = Height(X, Y + detail);
-				}
+				// Triangle 1: v1, v2, v3
+				vertexBuffer.put(X).put(Y).put(z1);
+				vertexBuffer.put(X).put(Y + detail).put(z2);
+				vertexBuffer.put(X + detail).put(Y + detail).put(z3);
 
-				/* Store the calculated coordinates */
-				v2[0] = x;
-				v2[1] = y;
-				v2[2] = z * scale;
-				// Get The (X, Y, Z) Value For The Top Right Vertex
-				x = X + detail;
-				if (cartesian) {
-					z = Height(X + detail + cx, Y + detail + cy);
-				} else {
-					z = Height(X + detail, Y + detail);
-				}
-				y = Y + detail;
+				// Triangle 2: v1, v3, v4
+				vertexBuffer.put(X).put(Y).put(z1);
+				vertexBuffer.put(X + detail).put(Y + detail).put(z3);
+				vertexBuffer.put(X + detail).put(Y).put(z4);
 
-				/* Store the calculated coordinates */
-				v3[0] = x;
-				v3[1] = y;
-				v3[2] = z * scale;
-				// Get The (X, Y, Z) Value For The Bottom Right Vertex
-				x = X + detail;
-				if (cartesian) {
-					z = Height(X + detail + cx, Y + cy);
-				} else {
-					z = Height(X + detail, Y);
-				}
-				y = Y;
+				// Normals for Triangle 1
+				float[] n1 = getGridNormal(gridY, gridX);
+				normalBuffer.put(-n1[0]).put(-n1[1]).put(-n1[2]);
 
-				v4[0] = x;
-				v4[1] = y;
-				v4[2] = z * scale;
+				float[] n2 = getGridNormal(gridY + 1, gridX);
+				normalBuffer.put(-n2[0]).put(-n2[1]).put(-n2[2]);
 
-				/*
-				 * We calculate the normals for the part of the plane and need
-				 * only 3 edges for a plane and the normal!
-				 */
-				result = calculateNormals(v1, v2, v3);
-				result2 = calculateNormals(v1, v3, v4);
+				float[] n3 = getGridNormal(gridY + 1, gridX + 1);
+				normalBuffer.put(-n3[0]).put(-n3[1]).put(-n3[2]);
 
-				if (inverse == false) {
-					gl.glTexCoord2f(tilex, tiley);
-					gl.glNormal3f(-result2[0], -result2[1], -result2[2]);
+				// Normals for Triangle 2
+				normalBuffer.put(-n1[0]).put(-n1[1]).put(-n1[2]);
+				normalBuffer.put(-n3[0]).put(-n3[1]).put(-n3[2]);
 
-					gl.glVertex3f(v1[0], v1[1], v1[2]);
+				float[] n4 = getGridNormal(gridY, gridX + 1);
+				normalBuffer.put(-n4[0]).put(-n4[1]).put(-n4[2]);
 
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley);
-					gl.glNormal3f(-result2[0], -result2[1], -result2[2]);
-					gl.glVertex3f(v4[0], v4[1], v4[2]);
-
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result2[0], -result2[1], -result[2]);
-					gl.glVertex3f(v3[0], v3[1], v3[2]);
-
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v3[0], v3[1], v3[2]);
-
-					gl.glTexCoord2f(tilex, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v2[0], v2[1], v2[2]);
-
-					gl.glTexCoord2f(tilex, tiley);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v1[0], v1[1], v1[2]);
-				}
-				/*
-				 * Inverse the z-coordinate for a correct geographic view of the
-				 * heightmap!
-				 */
-				else {
-					gl.glTexCoord2f(tilex, tiley);
-					gl.glNormal3f(-result2[0], -result2[1], -result2[2]);
-
-					gl.glVertex3f(v1[0], v1[1], -v1[2]);
-
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley);
-					gl.glNormal3f(-result2[0], -result2[1], -result2[2]);
-					gl.glVertex3f(v4[0], v4[1], -v4[2]);
-
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result2[0], -result2[1], -result[2]);
-					gl.glVertex3f(v3[0], v3[1], -v3[2]);
-
-					gl.glTexCoord2f(tilex + tileX / tilestepX, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v3[0], v3[1], -v3[2]);
-
-					gl.glTexCoord2f(tilex, tiley + tileY / tilestepY);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v2[0], v2[1], -v2[2]);
-
-					gl.glTexCoord2f(tilex, tiley);
-					gl.glNormal3f(-result[0], -result[1], -result[2]);
-					gl.glVertex3f(v1[0], v1[1], -v1[2]);
-
-				}
+				// Texture coordinates
+				texCoordBuffer.put(tilex).put(tiley);
+				texCoordBuffer.put(tilex).put(tiley + tileY / tilestepY);
+				texCoordBuffer.put(tilex + tileX / tilestepX).put(tiley + tileY / tilestepY);
+				texCoordBuffer.put(tilex).put(tiley);
+				texCoordBuffer.put(tilex + tileX / tilestepX).put(tiley + tileY / tilestepY);
+				texCoordBuffer.put(tilex + tileX / tilestepX).put(tiley);
 
 				i++;
-
+				gridX++;
 			}
-
 			i = 0;
-
 			u++;
+			gridY++;
 		}
 
-		u = 0;
+		vertexBuffer.rewind();
+		normalBuffer.rewind();
+		texCoordBuffer.rewind();
 
-		gl.glEnd();
+		int[] vbos = new int[3];
+		gl.glGenBuffers(3, vbos, 0);
 
+		// Vertex VBO
+		gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbos[0]);
+		gl.glBufferData(GL2.GL_ARRAY_BUFFER, vertexBuffer.capacity() * 4, vertexBuffer, GL2.GL_STATIC_DRAW);
+		gl.glVertexPointer(3, GL2.GL_FLOAT, 0, 0);
+
+		// Normal VBO
+		gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbos[1]);
+		gl.glBufferData(GL2.GL_ARRAY_BUFFER, normalBuffer.capacity() * 4, normalBuffer, GL2.GL_STATIC_DRAW);
+		gl.glNormalPointer(GL2.GL_FLOAT, 0, 0);
+
+		// TexCoord VBO
+		gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbos[2]);
+		gl.glBufferData(GL2.GL_ARRAY_BUFFER, texCoordBuffer.capacity() * 4, texCoordBuffer, GL2.GL_STATIC_DRAW);
+		gl.glTexCoordPointer(2, GL2.GL_FLOAT, 0, 0);
+
+		gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL2.GL_NORMAL_ARRAY);
+		gl.glEnableClientState(GL2.GL_TEXTURE_COORD_ARRAY);
+
+		if (bRender) {
+			gl.glDrawArrays(GL2.GL_TRIANGLES, 0, vertexCount);
+		}
+
+		gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glDisableClientState(GL2.GL_NORMAL_ARRAY);
+		gl.glDisableClientState(GL2.GL_TEXTURE_COORD_ARRAY);
+
+		gl.glDeleteBuffers(3, vbos, 0);
 		text.disable(gl);
-
 	}
 
 	public void renderDynamicHeightMap(GL2 gl) {
-
 		/* Get a reference to the textures!!! */
 		grid.textures.updateAnimRenderer();
 		int x, y;
@@ -361,8 +410,8 @@ public class HeightMaps {
 				v4[2] = z * scale;
 
 				/*
-				 * We calculate the normals for the part of the plane and need
-				 * only 3 edges for a plane and the normal!
+				 * We calculate the normals for the part of the plane and need only 3 edges for
+				 * a plane and the normal!
 				 */
 				result2 = calculateNormals(v1, v3, v4);
 				result = calculateNormals(v1, v2, v3);
@@ -392,8 +441,7 @@ public class HeightMaps {
 					gl.glVertex3f(v1[0], v1[1], v1[2]);
 				}
 				/*
-				 * Inverse the z-coordinate for a correct geographic view of the
-				 * heightmap!
+				 * Inverse the z-coordinate for a correct geographic view of the heightmap!
 				 */
 				else {
 					gl.glTexCoord2f(tilex, tiley);
@@ -538,14 +586,6 @@ public class HeightMaps {
 		return normalize(normal);
 	}
 
-	private float[] averageAndNormalize(float[] normal1, float[] normal2, float[] normal3, float[] normal4, float[] normal5, float[] normal6) {
-
-		float[] norm = { (normal1[0] + normal2[0] + normal3[0] + normal4[0] + normal5[0] + normal6[0]), (normal1[1] + normal2[1] + normal3[1] + normal4[1] + normal5[1] + normal6[1]),
-				(normal1[2] + normal2[2] + normal3[2] + normal4[2] + normal5[2] + normal6[2]) };
-
-		return normalize(norm);
-	}
-
 	/* Normalize a vector! */
 	private float[] normalize(float[] normal) {
 		float t = normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2];
@@ -560,16 +600,15 @@ public class HeightMaps {
 	public float Height(int x, int y) {
 		float value = 0;
 		/*
-		 * The default access to the ImageJ image values directly from the
-		 * image!
+		 * The default access to the ImageJ image values directly from the image!
 		 */
 		if (dynamicMap) {
 			value = HeightDynamic(x, y);
 
 		} else {
 			/*
-			 * We need this extra method to have a container for the height
-			 * values if we want to render a height map to ImageJ!
+			 * We need this extra method to have a container for the height values if we
+			 * want to render a height map to ImageJ!
 			 */
 
 			if (createMap) {
@@ -580,8 +619,7 @@ public class HeightMaps {
 
 			if (heights != null) {
 				/*
-				 * Important not to access values beyond the image width,
-				 * height!
+				 * Important not to access values beyond the image width, height!
 				 */
 				if (y < heights.length && x < heights[0].length && x >= 0 && y >= 0) {
 					value = heights[y][x];
