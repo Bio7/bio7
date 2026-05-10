ed582c4eb48ec140ae8bc81bbf0dbffb4b0eb021
 Bio7 <marcel.au@web.de>
2026-05-10 11:51:27 +0200

    Corrected header.

--- com.eco.bio7/src/com/eco/bio7/compile/CompileClassAndMultipleClasses.java
+++ com.eco.bio7/src/com/eco/bio7/compile/CompileClassAndMultipleClasses.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2025 M. Austenfeld
+ * Copyright (c) 2007-2026 M. Austenfeld
  * 
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
